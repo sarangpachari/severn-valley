be541a60859e11bd78c88dd607e5d3ae2300ac78
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-22 11:58:04 +0530

    some updates

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import trekkingImg from "../assets/trekking-main-img.webp";
 import sightSeeingImg from "../assets/sight-seeing-main.webp";
 import jungleSafariImg from "../assets/jungle-safari-main.webp";
+import teaGardenImg from "../assets/tea-garden-main.webp";
+import accomodationImg from "../assets/accomodation-main.webp"
+import yogaMeditationImg from "../assets/yoga-meditation-main.webp"
+
 
 const Services = () => {
   return (
@@ -14,24 +18,45 @@ const Services = () => {
         {/* CARDS CONTAINER */}
         <div className="flex flex-wrap justify-center">
           {/* CARD */}
-          <div className="w-1/2 p-3">
+          <div className="w-1/2 md:w-max p-3">
             <img src={trekkingImg} alt="" className="h-52 shadow-xl" />
             <p className="text-sm  font-bold mt-4 tracking-wide">
               Trekking & Adventures
             </p>
           </div>
           {/* CARD */}
-          <div className="w-1/2 p-3">
+          <div className="w-1/2 md:w-max p-3">
             <img src={sightSeeingImg} alt="" className="h-52 shadow-xl" />
             <p className="text-sm  font-bold mt-4 tracking-wide">
               Sight Seeing Tour
             </p>
           </div>
           {/* CARD */}
-          <div className="w-1/2 p-3">
+          <div className="w-1/2 md:w-max p-3">
             <img src={jungleSafariImg} alt="" className="h-52 shadow-xl" />
             <p className="text-sm  font-bold mt-4 tracking-wide">
-              Sight Seeing Tour
+              Jungle Safari
+            </p>
+          </div>
+          {/* CARD */}
+          <div className="w-1/2 md:w-max p-3">
+            <img src={teaGardenImg} alt="" className="h-52 shadow-xl" />
+            <p className="text-sm  font-bold mt-4 tracking-wide">
+              Tea Garden Visit
+            </p>
+          </div>
+          {/* CARD */}
+          <div className="w-1/2 md:w-max p-3">
+            <img src={accomodationImg} alt="" className="h-52 shadow-xl" />
+            <p className="text-sm  font-bold mt-4 tracking-wide">
+              Accomodation
+            </p>
+          </div>
+          {/* CARD */}
+          <div className="w-1/2 md:w-max p-3">
+            <img src={yogaMeditationImg} alt="" className="h-52 shadow-xl" />
+            <p className="text-sm  font-bold mt-4 tracking-wide">
+              Yoga and Meditation
             </p>
           </div>
         </div>
