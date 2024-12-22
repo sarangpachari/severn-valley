bf0bdd7c07c98d0a10f54e73e6c7c53bda5d22a2
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-22 19:27:36 +0530

    some updates 5

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -20,49 +20,75 @@ const HomeSlider = () => {
         showThumbs={false}
         showArrows={false}
       >
+        
         <div className="relative">
           <img src={slider1} className="lg:h-screen" />
           <div className=" absolute top-1/3 w-full">
-          <p className="lg:text-lg text-sm text-white m-5 tracking-widest">Discover the charm of Severn Valley—where every stay feels like home and every journey becomes an adventure.</p>
-            <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
-             <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
-            </button>
+            <p className="lg:text-lg text-sm text-white m-5 tracking-widest">
+              Discover the charm of Severn Valley—where every stay feels like
+              home and every journey becomes an adventure.
+            </p>
+            <a href="https://wa.me/+919562152795" target="_blank">
+              <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
+                <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
+              </button>
+            </a>
           </div>
         </div>
         <div className="relative">
           <img src={slider2} className="lg:h-screen" />
           <div className=" absolute top-1/3 w-full">
-          <p className="lg:text-lg text-sm text-white m-5 tracking-widest">Discover the charm of Severn Valley—where every stay feels like home and every journey becomes an adventure.</p>
-            <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
-             <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
-            </button>
+            <p className="lg:text-lg text-sm text-white m-5 tracking-widest">
+              Discover the charm of Severn Valley—where every stay feels like
+              home and every journey becomes an adventure.
+            </p>
+            <a href="https://wa.me/+919562152795" target="_blank">
+              <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
+                <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
+              </button>
+            </a>
           </div>
         </div>
         <div className="relative">
           <img src={slider3} className="lg:h-screen" />
           <div className=" absolute top-1/3 w-full">
-          <p className="lg:text-lg text-sm text-white m-5 tracking-widest">Discover the charm of Severn Valley—where every stay feels like home and every journey becomes an adventure.</p>
-            <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
-             <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
-            </button>
+            <p className="lg:text-lg text-sm text-white m-5 tracking-widest">
+              Discover the charm of Severn Valley—where every stay feels like
+              home and every journey becomes an adventure.
+            </p>
+            <a href="https://wa.me/+919562152795" target="_blank">
+              <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
+                <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
+              </button>
+            </a>
           </div>
         </div>
         <div className="relative">
           <img src={slider4} className="lg:h-screen" />
           <div className=" absolute top-1/3 w-full">
-          <p className="lg:text-lg text-sm text-white m-5 tracking-widest">Discover the charm of Severn Valley—where every stay feels like home and every journey becomes an adventure.</p>
-            <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
-             <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
-            </button>
+            <p className="lg:text-lg text-sm text-white m-5 tracking-widest">
+              Discover the charm of Severn Valley—where every stay feels like
+              home and every journey becomes an adventure.
+            </p>
+            <a href="https://wa.me/+919562152795" target="_blank">
+              <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
+                <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
+              </button>
+            </a>
           </div>
         </div>
         <div className="relative">
           <img src={slider5} className="lg:h-screen" />
           <div className=" absolute top-1/3 w-full">
-          <p className="lg:text-lg text-sm text-white m-5 tracking-widest">Discover the charm of Severn Valley—where every stay feels like home and every journey becomes an adventure.</p>
-            <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
-             <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
-            </button>
+            <p className="lg:text-lg text-sm text-white m-5 tracking-widest">
+              Discover the charm of Severn Valley—where every stay feels like
+              home and every journey becomes an adventure.
+            </p>
+            <a href="https://wa.me/+919562152795" target="_blank">
+              <button className="hover:bg-white hover:text-black px-4 py-3 lg:text-lg text-sm bg-gradient-to-r from-green-600 to-green-400 text-white rounded-full inline-flex items-center gap-2">
+                <FaWhatsapp className="text-xl md:text-3xl" /> Chat with Us
+              </button>
+            </a>
           </div>
         </div>
       </Carousel>
