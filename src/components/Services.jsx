9cad1a4f8f7a3d47c1d494e1a02f0c721842d8ad
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-22 09:42:25 +0530

    sight and trekking img uploaded

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import trekkingImg from "../assets/trekking-main-img.webp";
+import sightSeeingImg from "../assets/sight-seeing-main.webp";
 
 const Services = () => {
   return (
     <div>
-        <div className="p-5">
-            <h1 className="lg:text-3xl text-2xl font-bold lg:text-center">We Provides</h1>
-            <hr className='mt-2 w-3/4' />
+      <div className="p-5">
+        <h1 className="lg:text-3xl lg:font-medium text-2xl  tracking-tighter">
+          We <span className="text-emerald-800">Provides</span>
+        </h1>
+        <hr className="mt-2 mb-5 w-3/4" />
+        {/* CARDS CONTAINER */}
+        <div className="flex flex-wrap gap-5">
+          {/* CARD */}
+          <div className="w-60">
+            <img src={trekkingImg} alt="" className="h-96 shadow-xl" />
+            <p className="text-lg font-bold mt-4 tracking-wide">
+              Trekking & Adventures
+            </p>
+          </div>
+          {/* CARD */}
+          <div className="w-60">
+            <img src={sightSeeingImg} alt="" className="h-96 shadow-xl" />
+            <p className="text-lg font-bold mt-4 tracking-wide">
+              Sight Seeing Tour
+            </p>
+          </div>
+          
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
