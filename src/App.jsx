ac1d370f3c4e65329ec1b87d470fa4d90472760a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-28 22:58:59 +0530

    preloader 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,25 +11,46 @@ import JungleSafariGallery from "./pages/galleries/JungleSafariGallery";
 import TeaGardenGallery from "./pages/galleries/TeaGardenGallery";
 import Accomodation from "./pages/galleries/Accomodation";
 import YogaMeditationGallery from "./pages/galleries/YogaMeditationGallery";
+import { useEffect, useState } from "react";
+
+import Loader from "./components/loader/Loader";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
+    };
+
+    fakeDataFetch();
+  }, []);
+
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/gallery-tg" element={<TrekkingGallery />} />
-          <Route path="/gallery-ss" element={<SightSeeTourGallery />} />
-          <Route path="/gallery-js" element={<JungleSafariGallery />} />
-          <Route path="/gallery-teag" element={<TeaGardenGallery />} />
-          <Route path="/gallery-ac" element={<Accomodation />} />
-          <Route path="/gallery-ym" element={<YogaMeditationGallery />} />
-          <Route path="/*" element={<Pnf />} />
-        </Routes>
-        <Footer />
-      </Router>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/gallery-tg" element={<TrekkingGallery />} />
+              <Route path="/gallery-ss" element={<SightSeeTourGallery />} />
+              <Route path="/gallery-js" element={<JungleSafariGallery />} />
+              <Route path="/gallery-teag" element={<TeaGardenGallery />} />
+              <Route path="/gallery-ac" element={<Accomodation />} />
+              <Route path="/gallery-ym" element={<YogaMeditationGallery />} />
+              <Route path="/*" element={<Pnf />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </>
+      )}
     </>
   );
 }
