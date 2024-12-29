ffe82583ecac9003d483d0f6056817c0f7761d4a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-12-29 19:30:09 +0530

    update 6

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import trekkingImg from "../assets/trekking-main-img.webp";
 import sightSeeingImg from "../assets/sight-seeing-main.webp";
 import jungleSafariImg from "../assets/jungle-safari-main.webp";
